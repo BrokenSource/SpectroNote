a1fbc7f8a63003b3230eb3feeaec96a85cb4d1ce
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2025-02-27 07:56:46 -0400

    No need anymore to import resources and wrap on importlib

--- SpectroNote/__init__.py
+++ SpectroNote/__init__.py
@@ -1,9 +1,7 @@
-import SpectroNote.Resources as SpectroNoteResources
 from Broken import BrokenProject, __version__
 
 SPECTRONOTE = BrokenProject(
     PACKAGE=__file__,
     APP_NAME="SpectroNote",
     APP_AUTHOR="BrokenSource",
-    RESOURCES=SpectroNoteResources,
 )
