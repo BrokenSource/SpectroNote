0e89aa8f0fdacc0617543ddd5959216c584ebc92
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2025-05-30 19:22:36 -0400

    Remove profiler as it can be done externally

--- spectronote/__main__.py
+++ spectronote/__main__.py
@@ -1,12 +1,11 @@
 import sys
 
-from broken import BrokenProfiler
 from spectronote import SpectroScene
 
+
 def main():
-    with BrokenProfiler("SPECTRONOTE"):
-        spectronote = SpectroScene()
-        spectronote.cli(sys.argv[1:])
+    spectronote = SpectroScene()
+    spectronote.cli(sys.argv[1:])
 
 if __name__ == "__main__":
     main()
